01b5be524bf7d65925ecdc015cb5410a435d6273
Kal Academy <info@kalacademy.org>
2018-09-09 12:41:01 -0700

    unit tests

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CurrentWeatherComponent } from './current-weather.component';
+import { WeatherService } from '../weather/weather.service';
+import { WeatherServiceFake } from '../weather/weather.service.fake';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
@@ -8,7 +10,8 @@ describe('CurrentWeatherComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
+      declarations: [ CurrentWeatherComponent ],
+      providers: [{provide: WeatherService, useClass: WeatherServiceFake}]
     })
     .compileComponents();
   }));
@@ -19,7 +22,7 @@ describe('CurrentWeatherComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created',  () => {
     expect(component).toBeTruthy();
   });
 });
