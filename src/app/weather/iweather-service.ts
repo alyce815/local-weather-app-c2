874b59491e91a25acda393be978df74de15d85a9
Kal Academy <info@kalacademy.org>
2018-09-15 12:59:03 -0700

    city search component

--- src/app/weather/iweather-service.ts
+++ src/app/weather/iweather-service.ts
@@ -2,5 +2,5 @@ import { ICurrentWeather } from "../icurrent-weather";
 import { Observable } from "rxjs";
 
 export interface IWeatherService{
-  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather>
+  getCurrentWeather(search: string | number, country?: string): Observable<ICurrentWeather>
 }
